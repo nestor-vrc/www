bd3c10a49a6c9e5e7a074e2f0de8cdc2fe897c99
pyyupsk <pyyupsk@proton.me>
2024-10-13 14:22:41 +0700

    feat(config): add redirect to Discord in next.config.mjs
    Added a redirect for '/discord' to 'https://discord.gg/PaVYAuymgm' in Next.js config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                destination: 'https://discord.gg/PaVYAuymgm',
+                permanent: false,
+                source: '/discord',
+            },
+        ];
+    },
     swcMinify: true,
 };
 
